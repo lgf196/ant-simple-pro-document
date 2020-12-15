251182f5a18f0777c0dab7f3ef57b8c6ec240c24
lgf <18827634408@163.com>
2020-12-15 20:41:50 +0800

    update branch file

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-npm run docs:build
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-​git init
-git add -A
-git commit -m 'deploy'
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lgf196/ant-simple-pro-document.git master:gh-pages
-cd -
